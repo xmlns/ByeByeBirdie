5a5b7bac935a0df14376915f3e6223cdfb73faa6
Amogh Kulkarni <amogh4200@gmail.com>
2023-09-23 17:35:10 -0400

    Custom message requests

--- sample.py
+++ sample.py
@@ -5,7 +5,7 @@ import pyodbc
 import logging
 
 # Configure logging to write to a file named "unfollowers.log"
-logging.basicConfig(filename="unfollowers.log", level=logging.INFO, format="%(asctime)s %(message)s")
+logging.basicConfig(filename="C:\\Users\\amogh\\Desktop\\byebyebirdie.log'", level=logging.INFO, format="%(asctime)s %(message)s")
 
 # Authenticate to Twitter
 auth = tweepy.OAuthHandler("CONSUMER_KEY", "CONSUMER_SECRET")
@@ -20,12 +20,18 @@ bot_screen_name = api.me().screen_name
 # Create a dictionary to store the followers of each user who follows the bot
 followers_dict = {}
 
+# Create a dictionary to store the custom messages for each user who follows the bot
+messages_dict = {}
+
+# Define a hashtag to use for getting the user's input
+hashtag = "#byebyebirdie"
+
 # A connection string to connect to SQL Server and use a memory-optimized table
-conn_str = "Driver={SQL Server};Server=your_server;Database=your_database;Trusted_Connection=yes;"
+conn_str = "Driver={ODBC Driver 18 for SQL Server};Server=your_server;Database=your_database;Trusted_Connection=yes;"
 
 # Define a function to get the followers of each user who follows the bot and update the dictionary and the table
 def get_followers():
-    global followers_dict
+    global followers_dict, messages_dict
     # Loop through the users who follow the bot
     for user in tweepy.Cursor(api.followers, screen_name=bot_screen_name).items():
         # Get the screen name of the user
@@ -46,10 +52,29 @@ def get_followers():
         conn.close()
         # Update the dictionary with the current followers for the user
         followers_dict[screen_name] = current_followers
+        # Check if the user has already provided a custom message 
+        if screen_name not in messages_dict:
+            # If not, post a tweet to request a custom message 
+            tweet = api.update_status(f"Hello @{screen_name}, thank you for following @{bot_screen_name}. Please reply to this tweet with your custom message that you want me to use when someone unfollows you, and include {hashtag} in your reply.")
+            # Get the tweet ID 
+            tweet_id = tweet.id
+            # Wait for the user's reply 
+            reply = None
+            while not reply:
+                # Check if there is any new reply to the tweet 
+                for status in tweepy.Cursor(api.search, q=f"to:{bot_screen_name} {hashtag}", since_id=tweet_id).items():
+                    if status.in_reply_to_status_id == tweet_id:
+                        # Get the text of the reply 
+                        reply = status.text
+                        break
+                # Wait for 10 seconds before checking again 
+                time.sleep(10)
+            # Store the custom message in the dictionary 
+            messages_dict[screen_name] = reply
 
-# Define a function to check for unfollowers and tweet or DM them
+# Define a function to check for unfollowers and tweet or DM them with their custom message
 def check_unfollowers():
-    global followers_dict
+    global followers_dict, messages_dict
     # Loop through the users who follow the bot
     for user in tweepy.Cursor(api.followers, screen_name=bot_screen_name).items():
         # Get the screen name of the user
@@ -62,18 +87,18 @@ def check_unfollowers():
         unfollowers = previous_followers - current_followers
         # Update the dictionary with the current followers for the user
         followers_dict[screen_name] = current_followers
-        # If there are any unfollowers, tweet or DM them with a message and log it to a file 
+        # If there are any unfollowers, tweet or DM them with their custom message and log it to a file 
         if unfollowers:
-            # Compose a tweet or a DM with a summary of the unfollowers 
-            message = f"Bye bye {len(unfollowers)} accounts. You just unfollowed @{screen_name}. You will be missed. Not."
-            # Check if the user prefers to tweet or DM the message 
+            # Get the custom message from the dictionary 
+            message = messages_dict[screen_name]
+            # Check if the user prefers to tweet or DM their message 
             if tweet_preference:
-                # Post the tweet 
+                # Post their tweet 
                 api.update_status(message)
             else:
-                # Send the DM to the user 
+                # Send their DM to them 
                 api.send_direct_message(screen_name, message)
-            # Log the message to a file 
+            # Log their message to a file 
             logging.info(message)
 
 # Call the get_followers function for the first time to initialize the dictionary and the table 
